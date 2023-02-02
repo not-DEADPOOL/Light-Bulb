5c1a548e6dd019c15e754ee1f2d5a1cf1639b284
DEAD POOL <78198419+not-DEADPOOL@users.noreply.github.com>
2023-02-02 13:03:59 +0100

    Add files via upload

---
+++ Lib_light_bulb/Lib_light_bulb/LampadinaRegolabile.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Lib_light_bulb
+{
+    public class LampadinaRegolabile :Lampadina
+    {
+        int _pot;
+        public int potenza
+        {
+            get
+            {
+                return _pot;
+            }
+            set
+            {
+                _pot = value >= 100 ? 100 : (value < 1 ? 1 : value);
+            }
+        }
+
+        public void SetPower(int n)
+        {
+            if (acceso_spento)
+            {
+                pot = n;
+            }
+        }
+        public override string ToString()
+        {
+            string support;
+            for (int i = 0; i < length; i++)
+            {
+
+            }
+        }
+        //            ..........
+        //       .......::::::.......
+        //    .......:::======:::.......
+        //  .......::===========::........
+        // ......::===####@@####===::......
+        //......::===####@@@@####===::......
+        //.....::===####@@@@@@####===::.....
+        //.....::===#####@@@@#####===::.....
+        // .....::===#####@@#####===::.....
+        //  .....::====########====::.....
+        //   .....::=====####=====::.....
+        //     ....::============::....
+        //       ...::==========::...
+        //         ..::========::..
+        //           |__________|
+        //           <__________>
+        //           <__________>
+        //           <__________>
+        //            \________/
+    }
+}
---
+++ Lib_light_bulb/lampadina/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lib_light_bulb;
+namespace lampadina
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
